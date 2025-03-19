722debd914c681ce0d390d743b24c4a2b0babfe8
Oliver Melo <contato_limaverde@protonmail.com>
2025-03-19 00:41:19 -0300

    Matrix initialization and filling with Dyn. Mem. Alloc. at main(). Started creation of Matrix_Sum() function. .gitignore update for .vscode

--- basic_matrix_operations.c
+++ basic_matrix_operations.c
@@ -1,18 +1,79 @@
+/*  
+
+    The first-level pointer contains elements on a line, represents collumns;
+    The second-level pointer contains lines, represents lines.
+    "n-level" refers to how many layers of pointers;
+    First-level means one layer of pointer, i.e. one pointer, i.e. it points to an actual data;
+    Second-level means two layers of pointer, i.e. two pointers, i.e. it points to only one pointer;
+
+
+*/
+
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
-int Size_of_Array(int*array){
+int Size_of_Line(int** array){
 
-    return int size = sizeof(array) / sizeof(*array);
+    //to abstract away calculations of array size
+
+    int size = sizeof(array) / sizeof(*array);
+    
+    return size;
 
 }
 
-int** Multiply_Matrices(int**l_side_matrix, int**r_side_matrix, int**result_matrix){
+int Size_of_Collumn(int*array){
 
-    int count_l_lines = Size_of_Array(l_side_matrix);     //The first-level pointer contains elements on a line, represents collumns; 
-    int count_r_collumns = Size_of_Array(*r_side_matrix); //the second-level pointer contains lines, represents lines.
+    //to abstract away calculations of array size
 
-    if (count_l_lines!=count_r_collumns){ return result_matrix; }
+    int size = sizeof(array) / sizeof(*array);
+    
+    return size;
+
+}
+/*
+int** Sum_Matrices(int** L_matrix, int** matrix_B){
+
+    int** result_matrix;
+
+    {
+        int count_L_lines = Size_of_Line(L_matrix);
+        int count_L_collumns = Size_of_Collumn(*L_matrix);
+        int count_R_lines = Size_of_Line(matrix_B);
+        int count_R_collumns = Size_of_Collumn(*matrix_B);
+
+        if (count_L_lines != count_R_lines || count_L_collumns != count_R_collumns){
+        
+            return result_matrix;
+        
+        }
+
+    }
+
+    int count_lines = Size_of_Lines(L_matrix);
+    int count_collumns = Size_of_Collumn(*L_matrix);
+
+    for (int line = 0; line < count_lines; line++) {
+
+        for (int collumn = 0; collumn<count_collumns; collumn++){
+
+            result_matrix[line][collumn] = L_matrix[line][collumn] + matrix_B[line][collumn];
+
+        }
+    
+    }
+
+    return result_matrix
+
+}
+
+int** Multiply_Matrices(int** L_matrix, int** matrix_B, int** result_matrix){
+
+    int count_L_lines = Size_of_Line(L_matrix);      
+    int count_R_collumns = Size_of_Collumn(*matrix_B); //the second-level pointer contains lines, represents lines.
+
+    if (count_L_lines!=count_R_collumns){ return result_matrix; }
 
     
 
@@ -44,11 +105,83 @@ int** Multiply_Matrices(int**l_side_matrix, int**r_side_matrix, int**result_matr
     }
 
 }
-
+*/
 
 int main(){
 
+    /*
+    Declaring pointer of pointers that will serve as matrix;
+    Declaring int variables that will hold quantity of lines and collumns;
+    For matrix A and B
+    */
 
+    int** matrix_A;
+    int A_line_qtt, A_collumn_qtt;
+    
+    int**  matrix_B;
+    int B_line_qtt, B_collumn_qtt;
+
+    /*User inputs lines and collumns for A and B*/
+
+    scanf("%d", &A_line_qtt);
+    scanf("%d", &A_collumn_qtt);
+    
+    scanf("%d", &B_line_qtt);
+    scanf("%d", &B_collumn_qtt);
+
+    /*
+    Allocatting memory for the pointer of pointers;
+    i.e. creating array of pointers;
+    for matrix A and B
+    */
+
+    matrix_A = (int**)malloc( sizeof(int*) * A_line_qtt );
+
+    matrix_B = (int**)malloc( sizeof(int*) * B_line_qtt );
+
+    /*
+    Allocatting memory for each pointer inside the array of pointers;
+    i.e. creating arrays inside the array;
+    i.e. properly creating the matrix;
+    for matrix A and B
+    */
+    
+    for (int i=0; i<A_line_qtt; i++){
+    
+        matrix_A[i] = malloc(sizeof(int) * A_collumn_qtt);
+
+    }
+
+    
+    for (int i=0; i<B_line_qtt; i++){
+    
+        matrix_B[i] = malloc(sizeof(int) * B_collumn_qtt);
+    
+    }
+
+    //User inserts data in matrices A and B
+
+    printf("For matrix A:\n\n");
+
+    for (int i=0; i<A_line_qtt; i++){
+        for (int j=0; j<A_collumn_qtt; j++){
+
+            printf("Insert number in position a-%d-%d: ", i+1, j+1);
+            scanf("%d", &matrix_A[i][j]);
+        
+        }
+    }
+
+    printf("\nFor matrix B:\n\n");
+
+    for (int i=0; i<B_line_qtt; i++){
+        for (int j=0; j<B_collumn_qtt; j++){
+
+            printf("Insert number in position b-%d-%d: ", i+1, j+1);
+            scanf("%d", &matrix_B[i][j]);
+        
+        }
+    }
 
     return 0;
 }
